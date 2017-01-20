d1f62a4e3c7c6af647bb945d67158775daa00b25
whataboutdale <djarrett@dclibraries.org>
2017-01-20 13:19:51 -0700

    jumping pages

--- bootstrap-tour-ini.js
+++ bootstrap-tour-ini.js
@@ -12,62 +12,55 @@
         return $demo.removeClass("disabled", true);
       },
       debug: true,
-  steps: [
-  {
-    element: "#appSettings",
-    content: "Need a library card?",
-    placement: "bottom",
-	backdrop: true,
-  },
-  {
-    element: "#step2",
-    content: "Sign up here! You won't regret it.",
-    placement: "right",
-	backdrop: true,
-    onShow: function() {
-      return $("#appSettings").addClass("open");
-    },      
-    onHide: function() {
-      $("#appSettings").removeClass("open"); 
-    } 
-  } ,
-   {
-    element: "#step3",
-    content: "Stream and download digital movies, TV, music, magazines, e-books and audiobooks from this page.",
-    placement: "bottom",
-	backdrop: true,
-  }, 
-    {
-    element: "#step4",
-    content: "Got a new device? Need a digital assist? We're here for you!",
-    placement: "top",
-	backdrop: true,
-	path: "http://stage.dcl.org/digital-media/"
-  },   
-   {
-    element: "#appSettings2",
-    content: "Looking for new books and movies?",
-    placement: "bottom",
-	backdrop: true,
-  },
-  {
-    element: "#test3",
-    content: "Click New Titles to discover what's "Just Arrived" and what's "On Order."",
-    placement: "right",
-	backdrop: true,
-    onShow: function() {
-      return $("#appSettings2").addClass("open");
-    },      
-    onHide: function() {
-      $("#appSettings2").removeClass("open"); 
-    } 
-  } ,  
-    {
-    element: "#step5",
-    content: "You must register before using the new catalog, using your full 14-digit barcode. After that, you can create a user name for easy access on future visits! Your PIN is most likely the last four digits of your phone number. Happy searching! We hope you discover a great new read!",
-    placement: "bottom",
-	backdrop: true,
-  },  {
+      steps: [
+        {
+          path: "/",
+          element: "#demo",
+          placement: "bottom",
+          title: "Welcome to Bootstrap Tour!",
+          content: "Introduce new users to your product by walking them through it step by step."
+        }, {
+          path: "/",
+          element: "#usage",
+          placement: "top",
+          title: "A super simple setup",
+          content: "Easy is better, right? The tour is up and running with just a few options and steps."
+        }, {
+          path: "/",
+          element: "#license",
+          placement: "top",
+          title: "Best of all, it's free!",
+          content: "Yeah! Free as in beer... or speech. Use and abuse, but don't forget to contribute!"
+        }, {
+          path: "/api",
+          element: "#options",
+          placement: "top",
+          title: "Flexibilty and expressiveness",
+          content: "There are more options for those who want to get on the dark side.<br>\nPower to the people!",
+          reflex: true
+        }, {
+          path: "/api",
+          element: "#duration",
+          placement: "top",
+          title: "Automagically expiring step",
+          content: "A new addition: make your tour (or step) completely automatic. You set the duration, Bootstrap\nTour does the rest. For instance, this step will disappear in <em>5</em> seconds.",
+          duration: 5000
+        }, {
+          path: "/api",
+          element: "#methods table",
+          placement: "top",
+          title: "A new shiny Backdrop option",
+          content: "If you need to highlight the current step's element, activate the backdrop and you won't lose\nthe focus anymore!",
+          backdrop: true,
+          backdropPadding: 5
+        }, {
+          path: "/api",
+          element: "#reflex",
+          placement: "bottom",
+          title: "Reflex mode",
+          content: "Reflex mode is enabled, click on the text in the cell to continue!",
+          reflex: true
+        }, {
           path: "/api",
           title: "And support for orphan steps",
           content: "If you activate the orphan property, the step(s) are shown centered in the page, and you can\nforget to specify element and placement!",
