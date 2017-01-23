371ece6310154d73833b7db988747468e96fd3d9
whataboutdale <djarrett@dclibraries.org>
2017-01-23 11:46:10 -0700

    external js

--- bootstrap-tour-ini.js
+++ bootstrap-tour-ini.js
@@ -4,73 +4,66 @@
     $demo = $("#demo");
     duration = 5000;
     remaining = duration;
-    tour = new Tour({
-      onStart: function() {
-        return $demo.addClass("disabled", true);
-      },
-      onEnd: function() {
-        return $demo.removeClass("disabled", true);
-      },
-      debug: true,
-      steps: [
-        {
-          path: "/",
-          element: "#demo",
-          placement: "bottom",
-          title: "Welcome to Bootstrap Tour!",
-          content: "Introduce new users to your product by walking them through it step by step."
-        }, {
-          path: "/",
-          element: "#usage",
-          placement: "top",
-          title: "A super simple setup",
-          content: "Easy is better, right? The tour is up and running with just a few options and steps."
-        }, {
-          path: "/",
-          element: "#license",
-          placement: "top",
-          title: "Best of all, it's free!",
-          content: "Yeah! Free as in beer... or speech. Use and abuse, but don't forget to contribute!"
-        }, {
-          path: "/api",
-          element: "#options",
-          placement: "top",
-          title: "Flexibilty and expressiveness",
-          content: "There are more options for those who want to get on the dark side.<br>\nPower to the people!",
-          reflex: true
-        }, {
-          path: "/api",
-          element: "#duration",
-          placement: "top",
-          title: "Automagically expiring step",
-          content: "A new addition: make your tour (or step) completely automatic. You set the duration, Bootstrap\nTour does the rest. For instance, this step will disappear in <em>5</em> seconds.",
-          duration: 5000
-        }, {
-          path: "/api",
-          element: "#methods table",
-          placement: "top",
-          title: "A new shiny Backdrop option",
-          content: "If you need to highlight the current step's element, activate the backdrop and you won't lose\nthe focus anymore!",
-          backdrop: true,
-          backdropPadding: 5
-        }, {
-          path: "/api",
-          element: "#reflex",
-          placement: "bottom",
-          title: "Reflex mode",
-          content: "Reflex mode is enabled, click on the text in the cell to continue!",
-          reflex: true
-        }, {
-          path: "/api",
-          title: "And support for orphan steps",
-          content: "If you activate the orphan property, the step(s) are shown centered in the page, and you can\nforget to specify element and placement!",
-          orphan: true,
-          onHidden: function() {
-            return window.location.assign("/");
-          }
-        }
-      ]
-    }).init();
+var tour = new Tour({
+  debug: true,
+  storage: false,
+  steps: [
+  {
+	path:"/",
+    element: "#step1",
+    content: "Need a library card?",
+    placement: "bottom",
+	backdrop: true,
+  },
+  {
+	path:"/",
+    element: "#step2",
+    content: "Get it here, then use it like crazy!",
+    placement: "right",
+	backdrop: true,
+    onShow: function() {
+      return $("#step1").addClass("open");
+    },      
+    onHide: function() {
+      $("#step1").removeClass("open"); 
+    } 
+  } ,
+   {
+	path:"/",
+    element: "#step3",
+    content: "Stream and download digital movies, TV, music, magazines, e-books and audiobooks from this page.",
+    placement: "bottom",
+	backdrop: true,
+  }, 
+    {
+	path: "../digital-media",
+    element: "#help",
+    content: "Got a new device? Need a digital assist? We're here for you!",
+    placement: "top",
+	backdrop: true,
+  },   
+   {
+	path:"/",
+    element: "#step4",
+    content: "Create your account to start placing holds, renew checkouts, and more!",
+    placement: "bottom",
+	backdrop: true,
+  },
+	{
+	path:"/",
+    element: "#step5",
+    content: "It's easy! Use your full 14-digit barcode to start. Then create a username for easy access later. Hint: Your PIN is most likely the last four digits of your phone number.",
+    placement: "right",
+	backdrop: true,
+    onShow: function() {
+      return $("#step4").addClass("open");
+    },      
+    onHide: function() {
+      $("#step4").removeClass("open"); 
+    } 
+  }
+	  
+]}).init();
     if (tour.ended()) {
       $('<div class="alert alert-info alert-dismissable"><button class="close" data-dismiss="alert" aria-hidden="true">×</button>You ended the demo tour. <a href="#" data-demo>Restart the demo tour.</a></div>').prependTo(".content").alert();
     }
