03b540d289f93fbe01d682617304a96bb6f0dee4
mmorrow <mmorrow@dclibraries.org>
2017-09-06 10:42:33 -0600

    redo dale changes

--- list/collections.js
+++ list/collections.js
@@ -22,8 +22,17 @@ function getUrlParameter(name) {
 };
 function createAllPageTitle(title)
 {
-    var x = 
-    ' <h1 class="no-margin">' + title + '</h1>  ';
+	 var x = [
+     '<div class="text-center">',
+      '<h1 class="jumbo-text text-uppercase font-weight-400">What' + "'s"  + 'New</h1>',
+      ,<div class="separator-container padding-bottom-15">',
+       ' <div class="separator line-separator"><i class="fa fa-star-o"></i></div>',
+     ' </div>',
+   ' </div>',
+     ' <h1 class="no-margin">' + title + '</h1>  '
+	 ].join('\n');
+      
+  
       
     return x;
 }
