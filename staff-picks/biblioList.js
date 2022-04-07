6abe8f61c383e6c74a9b03b5fe3a7cf0db5eb66b
whataboutdale <whataboutdale@users.noreply.github.com>
2022-04-07 13:22:26 -0600

    setting up

--- staff-picks/biblioList.js
+++ staff-picks/biblioList.js
@@ -12,10 +12,7 @@ function createAllPageTitle(title)
 {
   var x = [
      '<div class="text-center">',
-      '<h1 class="jumbo-text text-uppercase font-weight-400 fade-in-text">What' + "'s"  + 'New</h1>',
-      '<div class="separator-container padding-bottom-15">',
-       ' <div class="separator line-separator"><i class="fas fa-star"></i></div>',
-     ' </div>',
+      '<h1 class="jumbo-text text-uppercase font-weight-400">What' + "'s"  + 'New</h1>',
    ' </div>',
      ' <h1 class="no-margin">' + title + '</h1>  '
 	 ].join('\n');
